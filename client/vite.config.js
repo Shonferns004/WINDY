1aeeccd2e7f704901fd949c83655c7b41880fe58
Shonferns004 <shawnferns004@gmail.com>
2026-01-23 00:58:19 +0530

    created a progressive web application

---
+++ client/vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vite.dev/config/
+export default defineConfig({
+  baseUrl: "./",
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["icons/icon-192.png", "icons/icon-512.png"],
+      manifest: {
+        name: "W I N D Y",
+        short_name: "Windy",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#f0f9f4",
+        theme_color: "#4ade80",
+        icons: [
+          {
+            src: "/icons/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
